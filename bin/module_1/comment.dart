61a2275177d7910fcbe5ed5a2b8a298c97c78f05
Rekan Dev <rekandev545@gmail.com>
2025-08-16 07:46:42 +0300

    add comment

---
+++ bin/module_1/comment.dart
@@ -0,0 +1,14 @@
+void main() {
+  // One Line Comment
+  // print('Ahmad');
+
+  // Multi Lines Comment
+  /* 
+  print('Ahmad');
+  print('Azad');
+  */
+
+  // Documentation Comment
+  /// This is code print "Omar"
+  // print('Omar');
+}
