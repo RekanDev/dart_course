f836da0c9cf26154cbd2db0087e26977b746a672
Rekan Dev <rekandev545@gmail.com>
2025-08-16 08:32:48 +0300

    add variables

---
+++ bin/module_1/variables.dart
@@ -0,0 +1,23 @@
+void main() {
+  var name = 'Ahmad';
+  print(name);
+
+  name = 'Azad';
+  print(name);
+
+  final age;
+  age = 35;
+  print(age);
+
+  // The final variable 'age' can only be set once.
+  // age = 30;
+
+  const isOpen = true;
+  print(isOpen);
+
+  // Constant variables can't be assigned a value after initialization.
+  // isOpen = false;
+
+  const pi = 3.1415926535897932;
+  print(pi);
+}
